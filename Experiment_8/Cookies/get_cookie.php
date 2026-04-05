e2ee6db1969ad6bbf426f1cc6be2a7363369c3fc
Shashank-NK <snkcoding23@gmail.com>
2026-04-05 17:56:17 +0530

    Correction

--- Experiment_8/Cookies/get_cookie.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_COOKIE['user'])) {
-    echo "Welcome back " . $_COOKIE['user'];
-} else {
-    echo "No cookie found!";
-}
-?>
--- Experiment_8/Session/session_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$username = $_POST['username'];
-
-$_SESSION['user'] = $username;
-
-header("Location: dashboard.php");
-?>
